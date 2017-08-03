e295db4dafd58cab37983715936677cbcada41c8
zhumenglimayer <657695558@qq.com>
2017-08-03 23:31:13 +0800

    spring-practice

---
+++ spring-1/src/com/mayer/spring/beans/autowire/Main.java
@@ -0,0 +1,16 @@
+package com.mayer.spring.beans.autowire;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("bean-autowire.xml");
+		
+		Person person = ctx.getBean(Person.class);
+		
+		System.out.println(person);
+	}
+
+}
