926aefb77609609a9434d83d71a68c44ce884c5d
zhumenglimayer <657695558@qq.com>
2017-08-04 20:09:35 +0800

    spring-practice

---
+++ spring-1/src/com/mayer/spring/beans/relation/Main.java
@@ -0,0 +1,26 @@
+package com.mayer.spring.beans.relation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.mayer.spring.beans.autowire.Address;
+import com.mayer.spring.beans.autowire.Person;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("bean-relation.xml");
+		
+		Address address = (Address) ctx.getBean("address2");
+		
+		System.out.println(address);
+		
+		address = (Address) ctx.getBean("address3");
+		
+		System.out.println(address);
+		
+		Person person = (Person) ctx.getBean("person");
+		System.out.println(person);
+	}
+
+}
