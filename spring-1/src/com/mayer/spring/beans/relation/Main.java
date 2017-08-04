11e28715a72ed0040b32db82db6bac2d54e19608
zhumenglimayer <657695558@qq.com>
2017-08-04 22:37:34 +0800

    spring-practice

--- spring-1/src/com/mayer/spring/beans/relation/Main.java
+++ spring-1/src/com/mayer/spring/beans/relation/Main.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.mayer.spring.beans.autowire.Address;
+import com.mayer.spring.beans.autowire.Car;
 import com.mayer.spring.beans.autowire.Person;
 
 public class Main {
@@ -21,6 +22,10 @@ public class Main {
 		
 		Person person = (Person) ctx.getBean("person");
 		System.out.println(person);
+		
+		Car car1 = (Car) ctx.getBean("car");
+		Car car2 = (Car) ctx.getBean("car");
+		System.out.println(car1==car2);
 	}
 
 }
