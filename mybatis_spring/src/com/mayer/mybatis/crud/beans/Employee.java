07b5f1f6442173d23678ab99f9f8dd507ac2aae5
zhumenglimayer <657695558@qq.com>
2017-09-11 22:09:54 +0800

    mybatis-spring

---
+++ mybatis_spring/src/com/mayer/mybatis/crud/beans/Employee.java
@@ -0,0 +1,85 @@
+package com.mayer.mybatis.crud.beans;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String lastName;
+	private String gender;
+	private String email;
+	private Department department;
+	
+	
+	
+	public Employee(Integer id, String lastName, String gender, String email, Department department) {
+		super();
+		this.id = id;
+		this.lastName = lastName;
+		this.gender = gender;
+		this.email = email;
+		this.department = department;
+	}
+
+
+	public Employee() {
+		super();
+	}
+	
+	
+	public Employee(Integer id, String lastName, String gender, String email) {
+		super();
+		this.id = id;
+		this.lastName = lastName;
+		this.gender = gender;
+		this.email = email;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public Department getDepartment() {
+		return department;
+	}
+
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", lastName=" + lastName + ", gender=" + gender + ", email=" + email
+				+ ", department=" + department + "]";
+	}
+
+
+	
+}
---
+++ mybatis_spring/src/com/mayer/mybatis/crud/dao/EmployeeMapper.java
@@ -0,0 +1,11 @@
+package com.mayer.mybatis.crud.dao;
+
+
+import com.mayer.mybatis.crud.beans.Employee;
+
+public interface EmployeeMapper {
+	
+	
+	public Employee getEmpById(Integer id);
+	
+}
