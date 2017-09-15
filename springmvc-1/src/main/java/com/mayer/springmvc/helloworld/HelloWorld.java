a5b4a66f363cc8afc8f440220571ed90ef9bff7e
zhumenglimayer <657695558@qq.com>
2017-09-15 23:51:08 +0800

    springmvc-practice

---
+++ springmvc-1/src/main/java/com/mayer/springmvc/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.mayer.springmvc.helloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorld {
+	
+	@RequestMapping("/helloworld")
+	public String hello(){
+		System.out.println("Hello World!!");
+		return "success";
+	}
+}
