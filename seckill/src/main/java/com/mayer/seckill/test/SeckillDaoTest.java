a1525c7144767659a15d548ba3163dd3fdec7a54
zhumenglimayer <657695558@qq.com>
2017-10-24 22:00:00 +0800

    seckill

---
+++ seckill/src/main/java/com/mayer/seckill/test/SeckillDaoTest.java
@@ -0,0 +1,28 @@
+package com.mayer.seckill.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SeckillDaoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testReduceNumber() throws Exception{
+		
+	}
+	
+	@Test
+	public void testQueryById() throws Exception{
+		
+	}
+	
+	@Test
+	public void testQueryAll() throws Exception{
+		
+	}
+}
