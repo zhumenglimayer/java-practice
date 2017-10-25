c0f8483d69863b9d8e1567e0256f14e9435ad97c
zhumenglimayer <657695558@qq.com>
2017-10-25 23:17:23 +0800

    seckill

--- seckill/src/main/java/com/mayer/seckill/test/SeckillDaoTest.java
+++ seckill/src/main/java/com/mayer/seckill/test/SeckillDaoTest.java
@@ -2,14 +2,25 @@ package com.mayer.seckill.test;
 
 import static org.junit.Assert.*;
 
+import javax.annotation.Resource;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+import com.mayer.seckill.dao.SeckillDao;
+import com.mayer.seckill.entity.Seckill;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:applicationContext.xml"})
 public class SeckillDaoTest {
+	
+	@Autowired
+	private SeckillDao seckillDao;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
 	
 	@Test
 	public void testReduceNumber() throws Exception{
@@ -18,7 +29,11 @@ public class SeckillDaoTest {
 	
 	@Test
 	public void testQueryById() throws Exception{
-		
+		System.out.println(seckillDao);
+//		long id = 1000;
+//		Seckill seckill = seckillDao.queryById(id);
+//		System.out.println(seckill.getName());
+//		System.out.println(seckill);
 	}
 	
 	@Test
